2d95e297c792083e291696c1e137d818e01ef639
Sumit Mangaonkar <mangaonkarsumit@gmail.com>
2015-04-08 18:56:35 +0530

    inital commit with application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestProject::Application.config.secret_token = '9f828245c5d3aeeeffe09b0d0ad67199000c1684d9b35434a735aa6ca7482c5f5db90345872bee5221fb9175ee7c1139fec385c57d39160f8a10b4f70a1a50ec'
